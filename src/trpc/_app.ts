db5bdf6610bb3ebe425f63fb9a8305f8178a132e
Deepak Kumar <deepakkumardash313@gmail.com>
2025-11-27 00:19:23 +0530

    Add : workflow trpc routes

--- src/trpc/_app.ts
+++ src/trpc/_app.ts
@@ -1,30 +1,8 @@
-import prisma from "@/lib/db";
-import { createTRPCRouter, protectedProcedure } from "./init";
-import { inngest } from "@/inngest/client";
-import { generateText } from 'ai';
-import { google } from '@ai-sdk/google';
-
+import { createTRPCRouter } from "./init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  getWorkflows: protectedProcedure.query(async () => {
-    return prisma.workflow.findMany()
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: { email: "a@b.com" }
-    })
-    return prisma.workflow.create({ data: { name: "test-workflow" } })
-  }),
-
-  testAi: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    })
-
-    return { success: true, message: "Job queued" }
-  })
+  workflows: workflowsRouter
 });
 
 export type AppRouter = typeof appRouter;
