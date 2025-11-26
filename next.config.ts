a54011fffe86419e02ef3c7e215bf43d694f836c
Deepak Kumar <deepakkumardash313@gmail.com>
2025-11-26 18:41:07 +0530

    Add : shadcn side bar component

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  devIndicators: false,
+  async redirects() {
+    return [{
+      source: "/", destination: "/workflows", permanent: false
+    }]
+  }
 };
 
 export default nextConfig;
